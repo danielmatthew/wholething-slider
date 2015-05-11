ab56217f4fe2293cc7316fb839dbd2636a28b1c4
Daniel Matthew <hello@danmatthew.co.uk>
2015-05-11 11:24:05 +0100

    Simple implementation of add/remove class

--- dan.js
+++ dan.js
@@ -11,7 +11,10 @@
 
 var breakpoints = {};
 
+// Our DOM element to modify
 var $feature = $('#feature-grid');
+
+// Store the classes to be assigned for each column
 var featureClasses = {
   col1: $feature.data('col1'),
   col2: $feature.data('col2'),
@@ -22,45 +25,46 @@ var featureClasses = {
 // Clone feature before slider is initialised
 var $cloned = $feature.clone();
 
+// Regular expression
+var re = /(feature-layout--[A-z]*)/g;
+
 
 enquire
+// Col1
 .register("(max-width: 20.74em)", {
   match: function() {
-    console.log("Screen is less than", "col1");
+    $feature.removeClass(featureClasses.col2, featureClasses.col3);
     $feature.addClass(featureClasses.col1);
-    // $feature.removeClass(featureClasses.col2, featureClasses.col3, featureClasses.col4);    
   },
   unmatch: function() {
     $feature.removeClass(featureClasses.col1);
   }
 })
-
-.register("(min-width: 20.75em)", {
-
+// Col2
+.register("(min-width: 25.25em)", {
   match: function() {
-    console.log("Screen is 25.75em", "col2");
+    $feature.removeClass(featureClasses.col1, featureClasses.col3);
     $feature.addClass(featureClasses.col2);
   },
-
   unmatch: function() {
     $feature.removeClass(featureClasses.col2);
   }
 })
-
+// Col3
 .register("(min-width: 50.5em)", {
-
-
   match: function() {
-    console.log("Screen is 50.5em!", "col3");
-    // $feature.removeClass();
-    $feature.addClass("feature-layout--slideshow");
+    $feature.removeClass(featureClasses.col1, featureClasses.col2);
+    $feature.addClass(featureClasses.col3);
     $feature.tbaSlider();
   },
-
   unmatch: function() {
     $feature.destroy();
-    // $feature.replaceWith($cloned).addClass('feature-layout--grid');
     $feature.removeClass(featureClasses.col3);
   }
-
 });
+
+var replaceClasses = function() {
+  $feature.attr('class', function(i, c) {
+    return c.replace(re, '');
+  });   
+};
